a092bbf0101182f1e48685318741587be64626cc
Jordon Kiger <jk582215@ohio.edu>
2020-02-02 14:34:48 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jk582215_MIS4200_2.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jk582215_MIS4200_2.Startup))]
+namespace jk582215_MIS4200_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
