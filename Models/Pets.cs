c3fc7d86c5e780f10e08da30886a7fa01bc06b08
Jordon Kiger <jk582215@ohio.edu>
2020-02-07 10:12:31 -0500

    added pet model

---
+++ Models/Pets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jk582215_MIS4200_2.Models
+{
+    public class Pets
+    {
+        public int petId { get; set; }
+        public string petType { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string ownerEmail { get; set; }
+        public string ownerPhone { get; set; }
+        public int MyProperty { get; set; }
+
+        public DateTime patientSince { get; set; }
+
+
+    }
+}
